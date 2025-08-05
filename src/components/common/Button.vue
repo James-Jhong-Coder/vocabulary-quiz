adaacb622a95aedd6ed48c83068ff9bbcaac65ad
JamesJhong <jamesjhong@star-link.tech>
2025-08-05 21:17:43 +0800

    feat: 🎸 add auto import package

---
+++ src/components/common/Button.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="box">
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped>
+@reference "@/assets/style/global.css";
+.box {
+    @apply flex items-center justify-center;
+    @apply p-4 text-gray-200 cursor-pointer;
+    border: 1px solid #f2f3f3;
+    width: 200px;
+}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
-
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
