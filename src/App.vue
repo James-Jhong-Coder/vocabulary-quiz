5362077c171dbd3c9fb68a41afe8e05f32486241
JamesJhong <jamesjhong@star-link.tech>
2025-08-05 21:59:55 +0800

    feat: 🎸 App.vue 加上 RouterView

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts"></script>
 
-<template></template>
+<template>
+    <div class="section">
+        <RouterView />
+    </div>
+</template>
 
-<style scoped></style>
+<style scoped>
+.section {
+    @apply w-full h-full;
+    @apply sm:w-[40rem] md:w-[48rem] mx-auto;
+    @apply bg-gray-300 pt-10;
+}
+</style>
