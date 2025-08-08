0b44dd365a89de61dbdd5005a4ce9fb577ea9be5
JamesJhong <jamesjhong@star-link.tech>
2025-08-08 10:51:09 +0800

    feat: 🎸 add Table & pagination component

---
+++ src/components/common/Table.vue
@@ -0,0 +1,19 @@
+<template>
+    <table class="table-auto">
+        <thead>
+            <tr>
+                <slot name="header"></slot>
+            </tr>
+        </thead>
+        <tbody>
+            <slot name="body"></slot>
+        </tbody>
+        <tfoot v-if="$slots.footer">
+            <tr>
+                <slot name="footer"></slot>
+            </tr>
+        </tfoot>
+    </table>
+</template>
+
+<script setup></script>
